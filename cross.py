bad3deebe506f6dbdc09a5419ec8a3947a1b3a1e
kimtaekang <k_2684@naver.com>
2022-10-02 16:29:07 +0900

    Chore : 파일 정리, ignore 변경

--- cross.py
+++ cross.py
@@ -5,44 +5,41 @@ from utils.datasets import letterbox
 from utils.general import non_max_suppression, scale_coords
 from utils.plots import Annotator
 
-# 횡단보도,신호등 모델 yolo v5
 MODEL_PATH = 'runs/train/exp4/weights/best.pt'
+MODEL_PATH2 = 'yolov5s.pt'
 
 img_size = 640
-conf_thres = 0.5  # confidence threshold 정확도
-iou_thres = 0.45  # NMS(Non-maximum Suppression) IOU threshold
+conf_thres = 0.5  # confidence threshold
+iou_thres = 0.45  # NMS IOU threshold
 max_det = 1000  # maximum detections per image
 classes = None  # filter by class
+classes2 = [0,1,2,3,5,7]  # filter by class
 agnostic_nms = False  # class-agnostic NMS
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device2 = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 ckpt = torch.load(MODEL_PATH, map_location=device)
+ckpt2 = torch.load(MODEL_PATH2, map_location=device2)
 model = ckpt['ema' if ckpt.get('ema') else 'model'].float().fuse().eval()
+model2 = ckpt2['ema' if ckpt2.get('ema') else 'model'].float().fuse().eval()
 class_names = ['횡단보도', '빨간불', '초록불'] # model.names
+class_names2 = ['사람','자전거','일반차량','오토바이','버스','트럭'] # model.names
 stride = int(model.stride.max())
+stride2 = int(model2.stride.max())
 colors = ((50, 50, 50), (0, 0, 255), (0, 255, 0)) # (gray, red, green)
+colors2 = ((0, 255, 255), (255, 0, 100), (255, 0, 0),(255, 0, 0),(255, 0, 0),(255, 0, 0)) # (yellow, purple, blue)
 
-# 차량,번호판 모델 yolo v4 다크넷 사용
-CONFIDENCE = 0.5
-THRESHOLD = 0.3
-LABELS = ['차량', '번호판']
-
-net = cv2.dnn.readNetFromDarknet('models/yolov4-ANPR.cfg', 'models/yolov4-ANPR.weights')
-
-# 동영상 로드
-cap = cv2.VideoCapture('data/full3.mp4')
+cap = cv2.VideoCapture('data/sample.mp4')
 
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 out = cv2.VideoWriter('data/output.mp4', fourcc, cap.get(cv2.CAP_PROP_FPS), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
 while cap.isOpened():
     ret, img = cap.read()
-    if not ret:  #frame없으면 종료
+    if not ret:
         break
 
-    H, W, _ = img.shape
-
-    # preprocess 전처리
+    #  횡단보도 preprocess
     img_input = letterbox(img, img_size, stride=stride)[0]
     img_input = img_input.transpose((2, 0, 1))[::-1]
     img_input = np.ascontiguousarray(img_input)
@@ -51,47 +48,46 @@ while cap.isOpened():
     img_input /= 255.
     img_input = img_input.unsqueeze(0)
 
-    # inference 횡단보도,신호등
+    # 횡단보도 inference
     pred = model(img_input, augment=False, visualize=False)[0]
 
-    # postprocess 후처리
+    # 횡단보도 postprocess
     pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)[0]
 
     pred = pred.cpu().numpy()
 
     pred[:, :4] = scale_coords(img_input.shape[2:], pred[:, :4], img.shape).round()
 
-    # inference 차량,번호판
-    blob = cv2.dnn.blobFromImage(img, scalefactor=1/255., size=(416, 416), swapRB=True)
-    net.setInput(blob)
-    output = net.forward()
+    # 사람, 차량 preprocess
+    img_input2 = letterbox(img, img_size, stride=stride2)[0]
+    img_input2 = img_input2.transpose((2, 0, 1))[::-1]
+    img_input2 = np.ascontiguousarray(img_input2)
+    img_input2 = torch.from_numpy(img_input2).to(device2)
+    img_input2 = img_input2.float()
+    img_input2 /= 255.
+    img_input2 = img_input2.unsqueeze(0)
 
-    boxes, confidences, class_ids = [], [], []
+    # 사람, 차량 inference
+    pred2 = model2(img_input2, augment=False, visualize=False)[0]
 
-    for det in output:
-        box = det[:4]
-        scores = det[4:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
 
-        if confidence > CONFIDENCE: # 차량 좌표구하는곳
-            cx, cy, w, h = box * np.array([W, H, W, H])
-            x = cx - (w / 2)
-            y = cy - (h / 2)
+    # 사람, 차량 postprocess
+    pred2 = non_max_suppression(pred2, conf_thres, iou_thres, classes2, agnostic_nms, max_det=max_det)[0]
 
-            boxes.append([int(x), int(y), int(w), int(h)])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
+    pred2 = pred2.cpu().numpy()
 
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE, THRESHOLD)
+    pred2[:, :4] = scale_coords(img_input2.shape[2:], pred2[:, :4], img.shape).round()
+
 
     # Visualize
     annotator = Annotator(img.copy(), line_width=3, example=str(class_names), font='data/malgun.ttf')
 
     cw_x1, cw_x2 = 0, 0 # 횡단보도 좌측(cw_x1), 우측(cw_x2) 좌표
 
+    # 횡단보도
     for p in pred:
         class_name = class_names[int(p[5])]
+
         x1, y1, x2, y2 = p[:4]
 
         annotator.box_label([x1, y1, x2, y2], '%s %d' % (class_name, float(p[4]) * 100), color=colors[int(p[5])])
@@ -99,26 +95,37 @@ while cap.isOpened():
         if class_name == '횡단보도':
             cw_x1, cw_x2 = x1, x2
 
-    if len(idxs) > 0:
-        for i in idxs.flatten():
-            class_name = LABELS[class_ids[i]]
-            x1, y1, w, h = boxes[i]
+    # 사람, 차량
+    for p in pred2:
+        try:
+            class_name = class_names2[int(p[5])]
+        except IndexError:
+            continue
 
-        alert_text = '' #TTS떄 가져올 부분
-        color = (255, 0, 0) # blue
+        x3, y3, x4, y4 = p[:4]
 
-        if class_name == '차량':
-            if (x1+w) < (cw_x1): # 왼쪽 차량
-                alert_text = str(cw_x1 - (x1+w)) + 'm 왼쪽 방향 '
-                color = (0, 0, 255) # red
-            elif x1 > cw_x2: # 오른쪽 차량
-                alert_text = str(x1 - cw_x2) + 'm 오른쪽 방향 '
-                color = (0, 0, 255) # red
-            elif cw_x1 < x1 < cw_x2 or cw_x1 < (x1+w) < cw_x2:
-                alert_text = '차량이 횡단보도에 있습니다. '
+        alert_text = ''
+        color = colors2[int(p[5])]
+
+        if class_name == '사람':
+            if cw_x1 < x4 < cw_x2 and y1 < y4 < y2:
+                alert_text = '사람이 횡단보도를 건너고 있습니다. '
+                color = (0, 255, 0) # green
+
+        if class_name == '일반차량' or class_name == '버스' or class_name == '트럭' or class_name == '오토바이' or class_name == '자전거':
+            if x4 < cw_x1: # 왼쪽 차량
+                alert_text = str(cw_x1 - (x4)) + 'm 왼쪽 방향 '
                 color = (255, 0, 0) # blue
+            elif x3 > cw_x2: # 오른쪽 차량
+                alert_text = str(x3 - cw_x2) + 'm 오른쪽 방향 '
+                color = (255, 0, 0) # blue
+            elif cw_x1 < x3 < cw_x2 or cw_x1 < x4 < cw_x2:
+                alert_text = class_name + '이 횡단보도에 있습니다.'
+                color = (0, 0, 255) # red
 
-        annotator.box_label([x1, y1, x1+w, y1+h], '%s %d' % (alert_text+class_name, confidences[i] * 100), color=color)
+
+        annotator.box_label([x3, y3, x4, y4], '%s %d' % (alert_text+class_name, float(p[4]) * 100), color=color)
+
 
     result_img = annotator.result()
 
